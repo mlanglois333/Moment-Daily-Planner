d436e47c543a794b1bddc6d7e7ae2d2cd5131660
Marc <mlanglois333@icloud.com>
2020-07-19 21:35:53 -0400

    created date objects

--- script.js
+++ script.js
@@ -1,7 +1,70 @@
 const currentDate = document.getElementById("currentDay");
-
-
-
 var now = moment();
-
 currentDate.innerHTML=now;
+var times = [
+    {
+        time: "9:00",
+        ampm: "am",
+        stored: "",
+    },
+    {
+        time: "10:00",
+        ampm: "am",
+        stored: "",
+    },
+    {
+        time: "11:00",
+        ampm: "am",
+        stored: "",
+    },
+    {
+        time: "12:00",
+        ampm: "pm",
+        stored: "",
+    },
+    {
+        time: "1:00",
+        ampm: "pm",
+        stored: "",
+    },
+    {
+        time: "2:00",
+        ampm: "pm",
+        stored: "",
+    },
+    {
+        time: "3:00",
+        ampm: "pm",
+        stored: "",
+    },
+    {
+        time: "4:00",
+        ampm: "pm",
+        stored: "",
+    },
+    {
+        time: "5:00",
+        ampm: "pm",
+        stored: "",
+    },
+    {
+        time: "6:00",
+        ampm: "pm",
+        stored: "",
+    },
+    {
+        time: "7:00",
+        ampm: "pm",
+        stored: "",
+    },
+    {
+        time: "8:00",
+        ampm: "pm",
+        stored: "",
+    },
+    {
+        time: "9:00",
+        ampm: "pm",
+        stored: "",
+    }
+]
